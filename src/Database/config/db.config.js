3c888ac3192466e121f857e1e80002cffcf035c3
Pro <andela@Pros-MacBook-Pro.local>
2022-02-10 08:01:16 +0200

    Create github actions tests

--- src/Database/config/db.config.js
+++ src/Database/config/db.config.js
@@ -1,15 +1,26 @@
-import mongoose from "mongoose"
+import { config } from 'dotenv';
+import mongoose from 'mongoose';
 
+config();
 
 const connectDB = async () => {
-        try {
-                const conn = await mongoose.connect(process.env.NODE_ENV === "production" ? process.env.MONGO_PROD_URL : process.env.NODE_ENV === 'test' ? process.env.MONGO_TEST_URL : process.env.MONGO_DEV_URL, {})
+	try {
+		const conn = await mongoose.connect(
+			process.env.NODE_ENV === 'production'
+				? process.env.MONGO_PROD_URL
+				: process.env.NODE_ENV === 'test'
+				? process.env.MONGO_TEST_URL
+				: process.env.MONGO_DEV_URL,
+			{ useNewUrlParser: true },
+		);
 
-                console.log('Connection has been established successfully...'.cyan.underline.bold);
-        } catch (err) {
-                console.log(`Error: ${err.message}`.red)
-                process.exit(1)
-        }
-}
+		console.log(
+			'Connection has been established successfully...'.cyan.underline.bold,
+		);
+	} catch (err) {
+		console.log(`Error: ${err.message}`.red);
+		process.exit(1);
+	}
+};
 
 export default connectDB;
